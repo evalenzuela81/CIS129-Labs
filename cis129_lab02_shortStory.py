beb8469083008960d63cb7506cf2838c6cd06bed
evalenzuela81 <evalenzuela81@mail.pima.edu>
2024-09-09 20:10:50 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Manny Valenzuela
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("Candy Man, Candy Man, Candy Man")
+print("After repeating candy man three times in the mirror I thought nothing would happen but the opposite")
+print("my bathroom light started flickering and weird noises started happening which had 10 year old me scared.")
+print("I started panickening but it did not stop anything it just continued I was scared for my life to the point of crying.")
+print("But obviously it was just a prank my older sister played on me but I could not conprehend at the time and it left me traumitized for a while.")
+print("Till this day I will never say any name in a mirror again from the treauma.")
