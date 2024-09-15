16ea4fa3ccffa90449dc860103a4a244275f656a
evalenzuela81 <evalenzuela81@mail.pima.edu>
2024-09-15 15:26:20 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -5,6 +5,8 @@ coffeePurchased = int(input("Number of Coffee's Bought?\n"))
 muffinsPurchased = int(input("Number of Muffin's Bought?\n"))
 #new items
 cakePurchased = int(input("Number of Cake's Bought?\n"))
+bagelsPurchased = int(input("Number of Bagel's Bought?\n"))
+
 print('***************************************\n\n***************************************')
 
 print('My Coffee and Muffin Receipt')
@@ -12,18 +14,21 @@ print('My Coffee and Muffin Receipt')
 coffee = int(5)
 muffin = int(4)
 cake = int(6)
+bagel = int(4)
 # multiplys the set price of coffee/muffins by the users input of how many were purchased
 c = coffee * coffeePurchased
 m = muffin * muffinsPurchased
 ck = cake * cakePurchased
+b = bagel * bagelsPurchased
 
 print(coffeePurchased,"Coffee's at $",coffee,'each: $',c)
 print(muffinsPurchased,"Mufin's at $",muffin,'each: $',m)
-print(cakePurchased,"Cake's at $",cake,'each: $', ck)
+print(cakePurchased,"Cake's at $",cake,'each: $',ck)
+print(bagelsPurchased,"Bagel's at $",bagel,'each: $',b)
 # takes total cost of the coffee/muffins and multiplies them by .06 to get the amount of tax money 
-tax = (float(c + m + ck) * .06)
+tax = (float(c + m + ck + b) * .06)
 # adds all the prices together the total cost of the coffee/muffins and tax cost
-total = (float(c + m + ck) + tax)
+total = (float(c + m + ck + b) + tax)
 
 print('6% tax: $', tax)
 print('---------')
